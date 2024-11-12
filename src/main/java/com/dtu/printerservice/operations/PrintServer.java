ae9ac8c3ee92ea75ab37ef0e789cff339ed8fdd2
cavras2009@hotmail.com <cavras2009@hotmail.com>
2024-11-12 19:24:15 +0100

    Added exceptions to print server to redirect on invalid token

--- src/main/java/com/dtu/printerservice/operations/PrintServer.java
+++ src/main/java/com/dtu/printerservice/operations/PrintServer.java
@@ -14,56 +14,113 @@ public class PrintServer implements PrinterOperations {
     public void print(String filename, String printerName, User user, String token) {
 
 
-        authentication.AuthenticateUser(Role.BASIC, token);
-        System.out.println("print called by user: XYZ");
+        try {
+            authentication.AuthenticateUser(Role.BASIC, token);
+            System.out.println("print called by user: XYZ");
+        } catch (RuntimeException e) {
+            System.out.println("Authentication failed: " + e.getMessage());
+            throw new RuntimeException("Invalid session. Please log in again.");
+        }
     }
 
     @Override
     public void queue(String printerName, User user, String token) {
-        authentication.AuthenticateUser(Role.ELEVATED, token);
-        System.out.println("queue called by user: XYZ");
+        try {
+
+
+            authentication.AuthenticateUser(Role.ELEVATED, token);
+            System.out.println("queue called by user: XYZ");
+        } catch (Exception e) {
+            System.out.println("Authentication failed: " + e.getMessage());
+            throw new RuntimeException("Invalid session. Please log in again.");
+        }
     }
 
     @Override
     public void topQueue(String printerName, int job, User user, String token) {
+        try {
+
+
         authentication.AuthenticateUser(Role.ELEVATED, token);
         System.out.println("topQueue called by user: XYZ");
+        } catch (Exception e) {
+            System.out.println("Authentication failed: " + e.getMessage());
+            throw new RuntimeException("Invalid session. Please log in again.");
+        }
     }
 
     @Override
     public void start(User user, String token) {
-        authentication.AuthenticateUser(Role.ADMIN, token);
 
+        try {
+
+
+        authentication.AuthenticateUser(Role.ADMIN, token);
         System.out.println("start called by user: XYZ");
+        } catch (Exception e) {
+            System.out.println("Authentication failed: " + e.getMessage());
+            throw new RuntimeException("Invalid session. Please log in again.");
+        }
     }
 
     @Override
     public void stop(User user, String token) {
+        try {
         authentication.AuthenticateUser(Role.ADMIN, token);
         System.out.println("stop called by user: XYZ");
+        } catch (Exception e) {
+            System.out.println("Authentication failed: " + e.getMessage());
+            throw new RuntimeException("Invalid session. Please log in again.");
+        }
     }
 
     @Override
     public void restart(User user, String token) {
+        try {
+
+
         authentication.AuthenticateUser(Role.ADMIN, token);
         System.out.println("restart called by user: XYZ");
+        } catch (Exception e) {
+            System.out.println("Authentication failed: " + e.getMessage());
+            throw new RuntimeException("Invalid session. Please log in again.");
+        }
     }
 
     @Override
     public void status(String printerName, User user, String token) {
+        try {
+
+
         authentication.AuthenticateUser(Role.ADMIN, token);
         System.out.println("status called by user: XYZ");
+        } catch (Exception e) {
+            System.out.println("Authentication failed: " + e.getMessage());
+            throw new RuntimeException("Invalid session. Please log in again.");
+        }
     }
 
     @Override
     public void readConfig(String parameter, User user, String token) {
+        try {
+
+
         authentication.AuthenticateUser(Role.ADMIN, token);
         System.out.println("readConfig called by user: XYZ");
+        } catch (Exception e) {
+            System.out.println("Authentication failed: " + e.getMessage());
+            throw new RuntimeException("Invalid session. Please log in again.");
+        }
     }
 
     @Override
     public void setConfig(String parameter, String value, User user, String token) {
-        authentication.AuthenticateUser(Role.ADMIN, token);
-        System.out.println("SetConfig called by user: XYZ");
+        try {
+            authentication.AuthenticateUser(Role.ADMIN, token);
+            System.out.println("SetConfig called by user: XYZ");
+        } catch (Exception e) {
+            System.out.println("Authentication failed: " + e.getMessage());
+            throw new RuntimeException("Invalid session. Please log in again.");
+        }
     }
 }
